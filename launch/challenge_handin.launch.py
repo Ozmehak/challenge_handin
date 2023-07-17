a60f5b837c7b5af8f7a76b6c8e1d52da393b0dd0
calle <calle.ozmehak@contrain.se>
2023-07-17 22:31:54 +0200

    added search script for diffbot launch in challenge launch

--- launch/challenge_handin.launch.py
+++ launch/challenge_handin.launch.py
@@ -4,13 +4,25 @@ from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.actions import Node
+import os
+
+def find_diffbot_launch():
+    install_folder = os.path.abspath(os.path.join(os.path.dirname(__file__), '../../../..'))
+    for root, dirs, files in os.walk(install_folder):
+        if 'diffbot.launch.py' in files:
+            return os.path.join(root, 'diffbot.launch.py')
+    return None
 
 def generate_launch_description():
+    diffbot_launch_path = find_diffbot_launch()
+    if diffbot_launch_path is None:
+        raise RuntimeError('Could not find diffbot.launch.py in install folder')
     return LaunchDescription([
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(diffbot_launch_path)
+        ),
         Node(
             package='challenge_handin',
             executable='velocity_converter.py',
         ),
-        
-    
-        ])
+    ])
